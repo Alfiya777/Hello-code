34718509dde5330f8b6874d9c77b66475985b49f
Alfiya <bukanovoi@ya.ru>
2022-12-10 15:33:08 +0500

    8-9 example

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}   //   массив 0  1  2  3  4  5  6  7  8
+int [] array = {11,21,31,41,15,61,17,18,19};
+
+int max1 = Max(
+    Max(a1, b1, c1)
+    Max(a2, b2, c2)
+    Max(a3, b3, c3));
+Console.WriteLine(max);
